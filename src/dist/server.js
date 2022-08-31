3ca132a11766c78e4c6f5723279e9f97b1b1e8ce
mateushls <mateushls01@gmail.com>
2022-08-31 08:41:32 -0300

    Adicionanda src

---
+++ src/dist/server.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.get('/', (request, response) => {
+    return response.json({ message: 'Olá Dev!' });
+});
+app.listen(3333, () => {
+    console.log('Server strarted on port 3333!');
+});
---
+++ src/src/routes/index.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+const routes = Router()
+
+/*
+routes.get('/', (request, response) => {
+    return response.sendFile(__dirname + '/templates/index.html')
+})*/
+
+
+export default routes
