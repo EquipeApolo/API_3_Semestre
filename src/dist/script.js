eac99c99ba99fec3dd78983c2a350ebbb13a09f6
Diego Batista <88885382+diiegobsilva@users.noreply.github.com>
2022-09-07 16:40:46 -0300

    Update script.js

--- src/dist/script.js
+++ src/dist/script.js
@@ -9,10 +9,13 @@ function dadosInput() {
     let speedAdditive = Number(document.getElementById('speedAdditive').value);
     let slope = Number(document.getElementById('slope').value);
     let altitude = Number(document.getElementById('altitude').value);
+
     var select = document.getElementById('btnCertification');
     var value = select.options[select.selectedIndex].value;
+
     var selectx = document.getElementById('btnCondition');
     var valuex = selectx.options[selectx.selectedIndex].value;
+    
     console.log(value);
     console.log(valuex);
     console.log(model, engine, temperature, weight, wind, braking, speedAdditive, slope, altitude);
