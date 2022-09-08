92aa8a065b3260c0fa45273705fa4ea886da75ab
Diego Batista <88885382+diiegobsilva@users.noreply.github.com>
2022-09-08 17:10:54 -0300

    Atualização CheckBox

--- src/public/script.ts
+++ src/public/script.ts
@@ -1,4 +1,6 @@
 function dadosInput(){  
+
+    //Pegando valor do Input
     let model = document.getElementById('model').value
     let temperature = Number (document.getElementById('temperature').value)
     let engine = document.getElementById('engine').value
@@ -9,23 +11,53 @@ function dadosInput(){
     let slope = Number( document.getElementById('slope').value)
     let altitude = Number (document.getElementById('altitude').value)
 
+
+    //Pegando valor do Dropbox Certification
 	var select = document.getElementById('btnCertification');
     var value = select.options[select.selectedIndex].value;
-
     if(value == 1){
         console.log('ANAC')
     }else if(value == 2){
         console.log('EASA')
-    }else{
+    }else if(value == 3){
         console.log('FAA')
     }
+    else{
+        console.log('Nada selecionado')
+    }
+    
 
+    //Pegando valor do Dropbox Runway condition
     var selectx = document.getElementById('btnCondition');
 	var valuex = selectx.options[selectx.selectedIndex].value;
 
-    console.log(document.getElementById('flapSwitch').value);
+
     
+    //Verificando Reverser Switch Check
+    let reverserSwitchCheck = false 
+    let checkboxRS = document.getElementById('reverserSwitch');
+        if(checkboxRS.checked) {
+           reverserSwitchCheck = true;}  
+
+        else{
+           reverserSwitchCheck;}
+
+
+    //Verificando Flap Switch Check
+    let flapSwitchCheck = false 
+    let checkboxFS = document.getElementById('flapSwitch');
+        if(checkboxFS.checked) {
+            flapSwitchCheck = true}  
     
+        else{
+            flapSwitchCheck}     
+
+
+            
+    //Input Resultado   
+    document.getElementById('Result').value = weight + wind;       
+
+
 
     console.log(valuex); 
     console.log(model, engine, temperature , weight, wind, braking, speedAdditive, slope, altitude)
