836ac8e83713aa2f87b26186be32311e00db1593
Diego Batista <88885382+diiegobsilva@users.noreply.github.com>
2022-11-17 10:59:31 -0300

    Update telaLogin.tsx

--- client/src/Views/telaLogin.tsx
+++ client/src/Views/telaLogin.tsx
@@ -3,72 +3,103 @@ import { useNavigate } from "react-router-dom";
 import Swal from 'sweetalert2'
 import { Component, useState } from "react";
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
-import Axios from "axios";
+import axios from "axios";
 
-const [Email, setEmail] = useState("");
-const [Senha, setSenha] = useState("");
-
-let params = {
-  email: Email,
-  senha: Senha,
+type state = {
+  dados: any[]
   }
+
 class TelaLogin extends Component<any>{
+  private email = ""
+  private senha = ""
+  private teste = []
+  private tpUser = ""
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dados: []
+    }
+    this.receberEmail = this.receberEmail.bind(this);
+    this.receberSenha = this.receberSenha.bind(this);
+  }
   
-  navigate = useNavigate()
 
-  receberEmail(evento: any) {
-      let entrada = evento.target.value;
-      setEmail(entrada)
+  receberEmail(event) {
+      const target = event.target
+      this.email = target.value;
   }
 
-  receberSenha(evento: any) {
-      let entrada = evento.target.value;
-      setSenha(entrada)
+  receberSenha(event) {
+    const target = event.target
+    this.senha = target.value;
   }
 
-  handleLogin(values: any) {
-      console.log(params);
-      values.preventDefault();
-      Axios.get(`http://localhost:3001/users/${params.email}`, {
-          params: { params }
-
+  componentDidMount(): void {
+      axios.get('http://localhost:3001/users').then(response => {
+        let dadosBanco = response.data
+        this.setState({
+          dados: dadosBanco
+        })
+        this.teste = dadosBanco
+        console.log(this.teste);
+        
       })
-          .then((response) => {
-              const data = response.data;
-              console.log(data);
-              debugger
-              if (data.length === 0) {
-                  Swal.fire({
-                      title: `Error`,
-                      html:
-                          ' <b>User not found</b>'
-                  })
-              } else if (data[0].senha_acesso === params.senha) {
-                  if (data[0].nivel_acesso === 1) {
-                      localStorage.setItem('idUsuario',`${data[0].id}`)
-                      localStorage.setItem('nomeUsuario',`${data[0].nome}`)
-                      localStorage.setItem('nivelAcesso',`${data[0].nivel_acesso
-                      }`)
-                      this.navigate("/Index")
-                  }
-                  else {
-                      localStorage.setItem('idUsuario',`${data[0].id}`)
-                      localStorage.setItem('nomeUsuario',`${data[0].nome}`)
-                      localStorage.setItem('nivelAcesso',`${data[0].nivel_acesso
-                      }`)
-                      this.navigate("/Calculo")
-                      
-                  }
-              } else if (data[0].senha_acesso !== params.senha) {
-                  Swal.fire({
-                      title: `Error`,
-                      html:
-                          ' <b>Incorrect Password</b> '
-                  })
-              }
-          });
-  };
+    }
 
+      login(){
+        this.teste.forEach((user) => {
+          console.log('TESTE');
+          if(user.email == this.email){
+            if(user.senha == this.senha){
+              if(user.tipoUsuario == "User"){
+                  this.tpUser = "User"
+              }else{
+                this.tpUser = "Adm"
+              }
+            }
+          }
+        }) 
+       console.log(this.tpUser);
+      }
+      
+
+
+
+          // .then(() => {
+          //     const data = this.teste;
+          //     console.log(data);
+          //     debugger
+          //     if (data.length === 0) {
+          //         Swal.fire({
+          //             title: `Error`,
+          //             html:
+          //                 ' <b>User not found</b>'
+          //         })
+          //     } else if (data[0].senha_acesso === this.senha) {
+          //         if (data[0].nivel_acesso === 1) {
+          //             localStorage.setItem('idUsuario',`${data[0].id}`)
+          //             localStorage.setItem('nomeUsuario',`${data[0].nome}`)
+          //             localStorage.setItem('nivelAcesso',`${data[0].nivel_acesso
+          //             }`)
+          //             this.navigate("/Index")
+          //         }
+          //         else {
+          //             localStorage.setItem('idUsuario',`${data[0].id}`)
+          //             localStorage.setItem('nomeUsuario',`${data[0].nome}`)
+          //             localStorage.setItem('nivelAcesso',`${data[0].nivel_acesso
+          //             }`)
+          //             this.navigate("/Calculo")
+                      
+          //         }
+          //     } else if (data[0].senha_acesso !== this.senha) {
+          //         Swal.fire({
+          //             title: `Error`,
+          //             html:
+          //                 ' <b>Incorrect Password</b> '
+          //         })
+          //     }
+          // });
 
 
     render() {
@@ -84,7 +115,7 @@ class TelaLogin extends Component<any>{
                     <Row>
                         <Col md={{ span: 6, offset: 3}}>
                             <h5 className="card-title"  style={{ marginTop: "30px" }}>Email</h5>
-                            <input type='text' className='input form-control form-control-lg inputGroup-sizing-sm' id="EmailUser"
+                            <input type='text' className='input form-control form-control-lg inputGroup-sizing-sm' id="EmailUser" onChange={this.receberEmail}
                             placeholder="Email"/>
                         </Col>
                     </Row>
@@ -92,14 +123,14 @@ class TelaLogin extends Component<any>{
                     <Row>
                         <Col md={{ span: 6, offset: 3}}>
                             <h5 className="card-title"  style={{ marginTop: "30px" }}>Password</h5>
-                            <input type='password' className='input form-control form-control-lg inputGroup-sizing-sm' id="SenhaUser"
+                            <input type='password' className='input form-control form-control-lg inputGroup-sizing-sm' id="SenhaUser" onChange={this.receberSenha}
                             placeholder="Password"/>
                         </Col>
                     </Row>
 
                     <Row>
                         <Col md={{ span: 6, offset: 3}}>
-                            <Button className="botao-resultado" style={{margin:'10px', marginTop:'15px'}} size="lg">Save</Button>
+                            <Button className="botao-resultado" style={{margin:'10px', marginTop:'15px'}} size="lg" onClick={this.login}>Save</Button>
                         </Col>
                     </Row>
 
