2c9a86ee7486a4b8fbf12e4789246c2aec12688d
Davi Elias Silvestre <davi.silvestre@pro4tech.com.br>
2022-11-25 13:19:35 -0300

    NavBar (falta o nome do usuário e Bem-Vindo)

--- client/src/Components/Nav.tsx
+++ client/src/Components/Nav.tsx
@@ -3,11 +3,17 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { useNavigate } from 'react-router-dom';
+import User from '../Models/user';
 
 
+function NavBar() {  
+  const history = useNavigate();
+  function LogOut(){
+    localStorage.clear();
+    history('./telaLogin');
 
-function NavBar() {
-  // let user = JSON.parse(localStorage.getItem('user-info'));
+  }
   return (
       <Navbar bg="primary" expand="lg">
       <Container>
@@ -16,16 +22,27 @@ function NavBar() {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-              <Nav.Link className='font-weight-bold' href="/calculation">Performance Calculation</Nav.Link>
-              <Nav.Link className='font-weight-bold' href="/registerAircraft">Aircraft Configuration</Nav.Link>
-              <Nav.Link className='font-weight-bold' href="/aircrafts">Aircrafts</Nav.Link>
-              <Nav.Link className='font-weight-bold' href="/users">Users</Nav.Link>
-          </Nav>
-          <NavDropdown title={
-            <span style={{opacity: 0.9, color: 'rgba(0,0,0,.5)'}} className='font-weight-bold'>User Name</span>
-            }>
-          </NavDropdown>
+        {
+          localStorage.getItem('nome') ?
+          <>
+            <Nav className="me-auto">
+              
+                  <Nav.Link className='font-weight-bold' href="/calculation">Performance Calculation</Nav.Link>
+                  <Nav.Link className='font-weight-bold' href="/registerAircraft">Aircraft Configuration</Nav.Link>
+                  <Nav.Link className='font-weight-bold' href="/aircrafts">Aircrafts</Nav.Link>
+                  <Nav.Link className='font-weight-bold' href="/flaps">Flaps</Nav.Link>
+                  <Nav.Link className='font-weight-bold' href="/users">Users</Nav.Link>
+
+            </Nav>
+            <NavDropdown title={
+              <span style={{opacity: 0.9, color: 'rgba(0,0,0,.5)'}} className='font-weight-bold'>User Name</span>
+              }>
+                <NavDropdown.Item onClick={LogOut}>Logout</NavDropdown.Item>
+            </NavDropdown>
+            </>
+            :
+            ""
+            }
         </Navbar.Collapse>
       </Container>
     </Navbar>
