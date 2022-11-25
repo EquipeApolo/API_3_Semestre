e4a807620f158faa9348fde7a198ce05dc2282a4
Davi Elias Silvestre <davi.silvestre@pro4tech.com.br>
2022-11-25 09:39:36 -0300

    NavBar (só pra pegar alt)

--- client/src/Components/Nav.tsx
+++ client/src/Components/Nav.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
+
+
 function NavBar() {
+  // let user = JSON.parse(localStorage.getItem('user-info'));
   return (
       <Navbar bg="primary" expand="lg">
       <Container>
         <Navbar.Brand href='/' style={{marginRight:10}}>
-          <img src="/embraer-1-logo-black-and-white.png" width="250" alt="Logo Embraer" />
+          <img src="/embraer-1-logo-black-and-white.png" width="200" alt="Logo Embraer" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
@@ -18,6 +22,10 @@ function NavBar() {
               <Nav.Link className='font-weight-bold' href="/aircrafts">Aircrafts</Nav.Link>
               <Nav.Link className='font-weight-bold' href="/users">Users</Nav.Link>
           </Nav>
+          <NavDropdown title={
+            <span style={{opacity: 0.9, color: 'rgba(0,0,0,.5)'}} className='font-weight-bold'>User Name</span>
+            }>
+          </NavDropdown>
         </Navbar.Collapse>
       </Container>
     </Navbar>
